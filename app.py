281950e3a25553b5d32ec63ab2ab3a50dee3c6d4
utpl-svramirez <svramirez@utpl.edu.ec>
2024-01-08 00:51:27 -0500

    act archivo

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 """
-    Ejemplo del manejo de hilos
+    Ejemplo del manejo de hilos - silvia ramirez
 """
 
+import os
 import requests
 import time
 import csv
 import threading
-# librería de python que permite ejecutar comandos
-import subprocess
+#import subprocess
+
+#Listado de sitios de educación en línea a nivel general
 
 def obtener_data():
     lista = []
     with open("informacion/data.csv") as archivo:
         lineas = csv.reader(archivo, quotechar="|")
+        
         for row in lineas:
-            # pass
-            # lista.append((numero, pagina))
-    # se retorna la lista con la información que se necesita
+            numero = row[0].strip()
+            url = row[1].strip()
+            lista.append((numero,url))
     return lista
 
 def worker(numero, url):
     print("Iniciando %s %s" % (threading.current_thread().getName(), url ))
-    # pass
+    pagina = requests.get(url)
+    nombre_archivo = f"{numero}.txt"
+    ruta_archivo = os.path.join("output",nombre_archivo)
+    with open(ruta_archivo,'w') as archivo:
+        archivo.writelines(pagina.txt)
+    print (f"Archivo guardado: {nombre_archivo}")
     time.sleep(10)
     print("Finalizando %s" % (threading.current_thread().getName()))
 
